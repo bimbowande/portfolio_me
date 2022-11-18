65e3def48bb1a5817322b8a15fbf3649661c4f6f
bimbowande <bimbowande@gmail.com>
2022-11-18 16:54:08 +0000

    update contact page

--- common/Contact/Input.tsx
+++ common/Contact/Input.tsx
@@ -4,11 +4,14 @@ import { TextArea,InputBox } from './style';
 export interface InputI{
     boxType?: string,
     inputType?: string,
-    placeholder?: string
+    placeholder?: string,
+    inputName: string,
+    changeValue?:(e:any) =>void,
+    inputValue?: any
 
 }
 
 export const Input: React.FC<InputI> = 
-(InputI) => InputI?.boxType === 'textArea' ? <TextArea  placeholder={InputI?.placeholder}></TextArea> : <InputBox placeholder={InputI?.placeholder}/>
+(InputI) => InputI?.boxType === 'textArea' ? <TextArea value={InputI?.inputValue} onChange={InputI?.changeValue} name={InputI?.inputName}  placeholder={InputI?.placeholder}></TextArea> : <InputBox value={InputI?.inputValue} onChange={InputI?.changeValue} name={InputI?.inputName} placeholder={InputI?.placeholder}/>
 
 
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { NextPage } from 'next';
 import { Layout } from '../components/Layout/Layout';
 import { PageHeader } from '../common/Project/styles';
 import { ContactBtn, InputGroup, PageSection } from '../common/Contact/style';
 import { Input } from '../common/Contact/Input';
 import { ButtonContainer } from '../common/Resume/Styles';
-import { Button } from '../stories/Button';
 
  const Contact: NextPage = () => {
+
+  const [emailContent, updateContent] = useState({name:'',email:'',message:''})
+
+  const handleChange = (event:any) => {
+    updateContent({ ...emailContent, [event.target.name]: event.target.value });
+  };
+
   return (
     <Layout>
         <div>
             <PageHeader>{`{`} Contact {`}`}</PageHeader>
             <PageSection>
               <InputGroup>
-                <Input placeholder='Enter Name'/>
+                <Input changeValue={(e:any)=>handleChange(e)}  inputName='name' placeholder='Enter Name'/>
               </InputGroup>
               <InputGroup>
-                <Input placeholder='Enter Email address'/>
+                <Input changeValue={(e:any)=>handleChange(e)} inputName='email' placeholder='Enter Email address'/>
               </InputGroup>
               <InputGroup>
-                <Input boxType='textArea' placeholder="Hi Bimbo, Nice Portfolio I'll like to discuss... "/>
+                <Input changeValue={(e:any)=>handleChange(e)} inputName='message' boxType='textArea' placeholder="Hi Bimbo, Nice Portfolio I'll like to discuss... "/>
               </InputGroup>
               <ButtonContainer>
                  <ContactBtn>Send Message</ContactBtn>
