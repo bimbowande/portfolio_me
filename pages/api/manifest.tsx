f61bcc49f55aa64f3ef4f9b82b1972e45b34785e
bimbowande <bimbowande@gmail.com>
2023-07-15 18:33:47 +0100

    resume page fixed

--- pages/api/manifest.tsx
+++ pages/api/manifest.tsx
@@ -7,7 +7,7 @@ export const Manifest : ManifestType = {
     personalInfo:{
         name:'Abimbola',
         surname: 'Oluwagbuyi',
-        summary:"Software Engineer with a demonstrated history of working in complex teams and on large web development projects. Skilled in software requirement analysis, design, development, deployment, and testing. I use ReactJs, TypeScript ,Angular, MongoDB and GraphQL",
+        summary:"A seasoned Frontend Engineer with over 5 years of experience, I am proficient in ReactJs, Angular, GraphQL, and SASS. With a passion for creating exceptional user experiences, I thrive in collaborative environments where I can apply my expertise in building dynamic and responsive interfaces. Whether working on complex web applications or implementing elegant UI designs, I am committed to delivering high-quality solutions that exceed expectations",
         title:'Frontend Engineer'
     },
 
@@ -15,34 +15,34 @@ export const Manifest : ManifestType = {
     projects:[
         {
             name:'Weather Wizard',
-            description:'Monitior the weather of most Capital Cities in the world',
+            description:'Monitior the weather of most Capital Cities in the world.',
             skills:['react-query', 'NodeJs','React','TypeScript','github'],
             link:'https://bimbowande.github.io/weather_wizard/'
         },
         {
             name:'GIGX',
-            description:'The secure and simple way to send and receive money across borders',
+            description:'The secure and simple way to send and receive money across borders.',
             skills:['NextJs', 'React','TypeScript','NodeJs','Redux','GraphQl'],
             link:'https://gigxpad.com'
 
         },
         {
             name:'PAKT',
-            description:'Creating a collaborative platform for community engagements ',
+            description:'Creating a collaborative platform for community engagements.',
             skills:['NextJs', 'React','TypeScript','NodeJs','Redux','GraphQl','Tailwind'],
             link:'https://pakt.world'
 
         },
         {
             name:'GIGO',
-            description:'Nigeria no. 1 fastest delivery app ',
+            description:'Nigeria no. 1 fastest delivery app.',
             skills:['NextJs', 'React','TypeScript','NodeJs','Redux','GraphQl','Tailwind','Angular'],
             link:'https://giglogistics.com'
 
         },
         {
             name:'Memmour',
-            description:"Shared gallery for event's pictures ",
+            description:"Shared gallery for event's pictures.",
             skills:['PHP', 'React','TypeScript','MYSQL','Jquery','Javascript','Tailwind','Angular'],
             link:''
 
@@ -70,7 +70,7 @@ export const Manifest : ManifestType = {
         'SQL',
         'MongoDB',
         'GraphQL',
-        'PostgresQl'
+        'PostgresQL'
     ],
 
     otherTechnologies: [
@@ -78,37 +78,38 @@ export const Manifest : ManifestType = {
         'Enzyme',
         'React Storybook',
         'Bootstrap',
-        'TailwindCSS'
+        'TailwindCSS',
+        'GitHub'
     ],
 
     experiences:[
         {
-            company:'AltoStream',
-            title:' Lead Frontend Engineer',
+            company:'Pakt',
+            title:' Senior Frontend Engineer',
             startDate:'Mar 2022 ',
             endDate:'Present',
-            accomplishment:'The striped hyena (Hyaena hyaena) is a species of hyena native to North and East Africa, the Middle East, the Caucasus, Central Asia and the Indian subcontinent. It is listed by the IUCN as near-threatened, as the global population is estimated to be under 10,000 mature individuals which continues to experience deliberate and incidental persecution along with a decrease in its'
+            accomplishment:'Optimizing the performance of the web platform by improving page load times and minimizing the number of HTTP requests. Designed a modern, responsive, and visually appealing user interface that provides an intuitive and easy-to-use experience for users'
         },
         {
             company:'GIGX',
             title:' Lead Frontend Engineer',
             startDate:'June 2021',
             endDate:'Mar 2022',
-            accomplishment:'The striped hyena (Hyaena hyaena) is a species of hyena native to North and East Africa, the Middle East, the Caucasus, Central Asia and the Indian subcontinent. It is listed by the IUCN as near-threatened, as the global population is estimated to be under 10,000 mature individuals which continues to experience deliberate and incidental persecution along with a decrease in its'
+            accomplishment:'Integrated the web application with blockchain APIs, such as Ethereum or Bitcoin, enables users to interact with the blockchain intuitively and user-friendly , Improved Data Analytics and visualization dashboard for stakeholders to measure progress and growth and company turnover'
         },
         {
             company:'GIGL',
             title:' Software Engineer',
             startDate:'Feb 2019',
             endDate:'Jun 2021',
-            accomplishment:'The striped hyena (Hyaena hyaena) is a species of hyena native to North and East Africa, the Middle East, the Caucasus, Central Asia and the Indian subcontinent. It is listed by the IUCN as near-threatened, as the global population is estimated to be under 10,000 mature individuals which continues to experience deliberate and incidental persecution along with a decrease in its'
+            accomplishment:' Participated in optimizing applications to ensure timely and high-quality delivery of functionality for GIG Logistics eCommerce solutions, which increased user engagement by 25%. Enhanced front-end performance through image optimization and clean coding practices, accelerating load times'
         },
         {
             company:'Planet Nest',
             title:' Full-stack Engineer',
             startDate:'May 2017',
             endDate:'Oct 2019 ',
-            accomplishment:'The striped hyena (Hyaena hyaena) is a species of hyena native to North and East Africa, the Middle East, the Caucasus, Central Asia and the Indian subcontinent. It is listed by the IUCN as near-threatened, as the global population is estimated to be under 10,000 mature individuals which continues to experience deliberate and incidental persecution along with a decrease in its'
+            accomplishment:'I excellently translated UI/UX design wireframes to actual code that will produce visual elements of the application, Executed and introduced the use of version control tools like GitLab and GitHub, Functioned as a Facilitator in a Bootcamp with 100+ undergraduates in attendance, Ensured that security and quality assurance best practices are followed throughout the entire software development cycle'
         }
 
     ],
