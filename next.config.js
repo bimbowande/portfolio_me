2729bef58f996f59c9e87bb751c4d7d9352fffdb
bimbowande <oluwagbuyi01@gmail.com>
2026-05-05 15:51:57 +0100

    update changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   swcMinify: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
@@ -13,28 +14,4 @@ const nextConfig = {
 module.exports = nextConfig
 
 
-// const path = require('path');
 
-// const withImages = require('next-images');
-// module.exports = withImages({
-//   fileExtensions: ['jpg', 'jpeg', 'png', 'gif', 'svg'],
-//   disableStaticImages: true,
-//   webpack: config => {
-//     config.resolve.modules.push(path.resolve('./src'));
-//     config.module.rules.push(
-//       ...[
-//         {
-//           test: /\.(eot|otf|ttf|woff|woff2)$/,
-//           use: {
-//             loader: 'url-loader',
-//             options: {
-//               limit: 1000000
-//             }
-//           }
-//         }
-//       ]
-//     );
-
-//     return config;
-//   }
-// });
